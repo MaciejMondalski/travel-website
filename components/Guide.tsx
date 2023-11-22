c24f069f9531e5947205c3322638dd0a12ba1a03
MaciejMondalski <maciej.mondalski@gmail.com>
2023-11-22 23:58:39 +0100

    fixed meter image

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -22,20 +22,22 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className='flexCenter max-container relative w-full border-2 border-blue-500'>
+      <div className='flexCenter max-container relative w-full'>
         <Image
           src='/boat.png'
           alt='boat'
           width={1440}
           height={580}
-          className='2-full object-cover object-center 2xl:rounded-5xl'
+          className='w-full object-cover object-center 2xl:rounded-5xl'
         />
-        <div className='absolute bg-white py-8 pl-5 pr-7 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
+
+        <div className='absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
           <Image
             src='/meter.svg'
             alt='meter'
             width={16}
             height={158}
+            className='h-full w-auto'
           />
           <div className='flexBetween flex-col'>
             <div className='flex w-full flex-col'>
@@ -45,9 +47,10 @@ const Guide = () => {
               </div>
               <p className='bold-20 mt-2'>Aguas Calientes</p>
             </div>
+
             <div className='flex w-full flex-col'>
-              <p className='regular-16 text-gray-20'>Start Track</p>
-              <p className='bold-20 mt-2 whitespace-nowrap'>Wonorejo Pasuruan</p>
+              <p className='regular-16 text-gray-20'>Start track</p>
+              <h4 className='bold-20 mt-2 whitespace-nowrap'>Wonorejo Pasuruan</h4>
             </div>
           </div>
         </div>
