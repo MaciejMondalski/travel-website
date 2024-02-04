67f6f72fc9e6b51035fd5e4cb9948d8f9fb977a8
MaciejMondalski <maciej.mondalski@gmail.com>
2024-02-05 02:35:21 +0700

    burger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,46 +1,80 @@
+'use client';
+
 import { NAV_LINKS } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from './Button';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <nav className='flexBetween max-containter padding-container relative z-30 py-5'>
-      <Link href='/'>
-        <Image
-          src='/hilink-logo.svg'
-          alt='logo'
-          width={74}
-          height={29}
-        />{' '}
-      </Link>
+  const [menuToggle, setMenuToggle] = useState(false);
 
-      <ul className='hidden h-full gap-12 lg:flex'>
-        {NAV_LINKS.map((link) => (
-          <Link
-            className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
-            href={link.href}
-            key={link.key}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <div className='lg:flexCenter hidden'>
-        <Button
-          type='button'
-          title='Login'
-          icon='/user.svg'
-          variant='btn_dark_green'
-        />
+  return (
+    <nav className=''>
+      <div className='flexBetween padding-container max-containter  relative  z-30 py-5'>
+        <Link href='/'>
+          <Image
+            src='/hilink-logo.svg'
+            alt='logo'
+            width={74}
+            height={29}
+          />{' '}
+        </Link>
+        <ul className='hidden h-full gap-12 lg:flex'>
+          {NAV_LINKS.map((link) => (
+            <Link
+              className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
+              href={link.href}
+              key={link.key}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+        <div className='lg:flexCenter hidden'>
+          <Button
+            type='button'
+            title='Login'
+            icon='/user.svg'
+            variant='btn_dark_green'
+          />
+        </div>
+        <button onClick={() => setMenuToggle(!menuToggle)}>
+          {!menuToggle ? (
+            <Image
+              src='menu.svg'
+              alt='menu'
+              width={32}
+              height={32}
+              className='inline-block cursor-pointer lg:hidden'
+            />
+          ) : (
+            <Image
+              src='menu-close.svg'
+              alt='menu'
+              width={32}
+              height={32}
+              className='inline-block cursor-pointer lg:hidden'
+            />
+          )}
+        </button>
       </div>
-      <Image
-        src='menu.svg'
-        alt='menu'
-        width={32}
-        height={32}
-        className='inline-block cursor-pointer lg:hidden'
-      />
+
+      {/* Burger Menu */}
+
+      {menuToggle && (
+        <ul className='bg-white-600 z-100  flex-col flexCenter gap-8 lg:hidden static duration-500 '>
+          {NAV_LINKS.map((link) => (
+            <Link
+              className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
+              href={link.href}
+              key={link.key}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
